8b697e55a13a1fda8a8cdc9531011f9109b0505d
Roy Kim <rykim79@gmail.com>
2018-09-12 12:54:29 -0400

    add #take() to Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,12 @@
 class Player
   attr_reader :name, :hand
 
-  def initialize(name = 'John')
+  def initialize(name: 'John')
     @name = name
     @hand = []
+  end
+
+  def take(cards)
+    cards.each { |card| hand.push(card)  }
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,11 @@
 require 'rspec'
 require 'player'
+require 'playing_card'
 
 describe Player do
-  let(:player) { Player.new('Bob') }
+  let(:player) { Player.new(name: 'Bob') }
+  let(:card1) { PlayingCard.new(rank: 'A', suit: 'Spades') }
+  let(:card2) { PlayingCard.new(rank: 'K', suit: 'Hearts') }
 
   describe '#initialize' do
     it 'sets the players name' do
@@ -11,6 +14,19 @@ describe Player do
 
     it 'starts the player with and empty hand' do
       expect(player.hand.count).to eq 0
+    end
+  end
+
+  describe '#take' do
+    it 'adds a PlayingCard to the players hand' do
+      player.take([card1])
+      expect(player.hand.count).to eq 1
+    end
+
+    it 'can add multiple cards' do
+      cards = [card1, card2]
+      player.take(cards)
+      expect(player.hand.count).to eq 2
     end
   end
 end
